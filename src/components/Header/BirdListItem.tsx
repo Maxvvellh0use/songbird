d376d71980933d50193b260e0978efd9121e6143
Maksim Pavlov <pavlovmark100@gmail.com>
2020-07-31 23:06:33 +0400

    feat: add functional active classes to BirdListItem component

--- src/components/Header/BirdListItem.tsx
+++ src/components/Header/BirdListItem.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import { birdsCategories } from "../../data/birdsCategories";
 
 export const BirdListItem: () => JSX.Element[] = () => {
+    const arrayClassState: string[] = new Array(birdsCategories.length).fill('');
+    const [activeListClass, setActiveListClass] = useState(arrayClassState);
+    const clickNavHandler = (event: any) => {
+        activeListClass.map(() => {
+            const newArrayClassState = [...arrayClassState];
+            newArrayClassState[event.target.dataset.targetIndex] = ' bird_list_item__active';
+            return setActiveListClass(newArrayClassState);
+        })
+    }
     return (
-        birdsCategories.map(birdsCategory => {
-           return <li key={birdsCategory} className="bird_list_item">{birdsCategory}</li>
+        birdsCategories.map((birdsCategory, index) => {
+            return <li onClick={clickNavHandler}
+                       key={birdsCategory}
+                       data-target-index={index}
+                       className={"bird_list_item" + activeListClass[index]}>{birdsCategory}</li>
         })
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import { BirdListItem } from "./BirdListItem";
 
