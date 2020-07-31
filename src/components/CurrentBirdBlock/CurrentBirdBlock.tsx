bfbbb5528f08546b26c11d2fa7a4a4a22ed87068
Maksim Pavlov <pavlovmark100@gmail.com>
2020-07-31 23:39:40 +0400

    feat: add alt bird image

--- src/components/CurrentBirdBlock/CurrentBirdBlock.tsx
+++ src/components/CurrentBirdBlock/CurrentBirdBlock.tsx
@@ -1,11 +1,13 @@
 import { AudioPlayerBird } from "./AudioPlayerBird";
-import './current-bird.scss'
+import './current-bird.scss';
+import altBirdImage from '../../assets/img/alt_bird_image.png';
 import React from "react";
 
 export const CurrentBirdBlock: React.FunctionComponent = () => {
     return (
         <div className='wrapper'>
-            <div className="current_bird_block">
+            <div className='current_bird_block'>
+                <img className='image_bird' src={altBirdImage}/>
                 <AudioPlayerBird />
             </div>
         </div>
